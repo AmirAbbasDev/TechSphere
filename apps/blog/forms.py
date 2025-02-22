a24ba354ebbe48f05fc6098f38e718652ea90192
Amir Abbas <aamirabbas.dev@gmail.com>
2025-02-22 12:35:01 +0500

    added variable filter for turncate article on home page

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -16,3 +16,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, widget=forms.Textarea)
